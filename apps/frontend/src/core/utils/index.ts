db5439e6db8e7ee7dab5710307b7c78e0f3a7d0a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-19 13:47:37 -0300

    feat: import utils class

---
+++ apps/frontend/src/core/utils/index.ts
@@ -0,0 +1,6 @@
+import Id from "./Id.class";
+import Alias from "./Alias.class";
+import Password from "./Password.class";
+import DateFormatter from "./DateFormatter.class";
+
+export { Id, Alias, Password, DateFormatter };
