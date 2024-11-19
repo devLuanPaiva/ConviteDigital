7d225e6568ccbe926b0e7bc884b175e036f7c482
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-19 14:14:23 -0300

    feat: export functions and model of Guest

---
+++ apps/frontend/src/core/guests/index.ts
@@ -0,0 +1,7 @@
+import Guest from './model/Guests.model';
+import createNullGuest from './functions/createNullGuest';
+import complementGuest from './functions/complementGuest';
+import validateGuest from './functions/validateGuest';
+
+export { complementGuest, createNullGuest, validateGuest };
+export type { Guest };
