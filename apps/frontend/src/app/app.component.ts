321dd97b7d58df06aabc409f020e2997690a8186
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-20 14:56:33 -0300

    refactor: add page component template in the app component

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { PageComponent } from './components/template/page/page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-}
+export class AppComponent {}
