df864e39e53e16fa6c732fc075fd42c6c05f8b9a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-05 15:01:02 -0300

    feat: create landing page

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { PageComponent } from './components/template/page/page.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { BigLogoComponent } from './components/template/big-logo/big-logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PageComponent],
+  imports: [RouterModule, CommonModule, BigLogoComponent],
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None,
 })
