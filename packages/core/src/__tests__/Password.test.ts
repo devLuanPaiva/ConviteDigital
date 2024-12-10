8f004db025ec13abf8f5ad3f4d0cae1c596e25ee
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-10 16:10:11 -0300

    feat: testing the generating a password with a custom size

--- packages/core/src/__tests__/Password.test.ts
+++ packages/core/src/__tests__/Password.test.ts
@@ -4,4 +4,9 @@ describe("Password", () => {
     const password = Password.new();
     expect(password.length).toBe(15);
   });
+  it('should generate a password with a custom size', () => {
+    const customSize = 20;
+    const password = Password.new(customSize);
+    expect(password).toHaveLength(customSize);
+  });
 });
