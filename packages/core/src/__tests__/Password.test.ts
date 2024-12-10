a88f78b59e9749f6b8400a86c4a71d862c21c95e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-10 16:12:24 -0300

    feat: testing the generating different passwords on consecutive calls

--- packages/core/src/__tests__/Password.test.ts
+++ packages/core/src/__tests__/Password.test.ts
@@ -4,18 +4,23 @@ describe("Password", () => {
     const password = Password.new();
     expect(password.length).toBe(15);
   });
-  it('should generate a password with a custom size', () => {
+  it("should generate a password with a custom size", () => {
     const customSize = 20;
     const password = Password.new(customSize);
     expect(password).toHaveLength(customSize);
   });
-  it('should generate a password containing only allowed characters', () => {
+  it("should generate a password containing only allowed characters", () => {
     const allowedCharacters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-{}[]|\\:;"\'<>,.?/~`';
-    const password = Password.new(50); 
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-{}[]|\\:;\"'<>,.?/~`";
+    const password = Password.new(50);
 
     for (const char of password) {
       expect(allowedCharacters).toContain(char);
     }
+  });
+  it("should generate different passwords on consecutive calls", () => {
+    const password1 = Password.new();
+    const password2 = Password.new();
+    expect(password1).not.toEqual(password2);
   });
 });
