41a9a2552a0f446e60ffc07892b6ea489a43346a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-10 15:34:10 -0300

    fix: add standalony

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { BigLogoComponent } from './components/template/big-logo/big-logo.compon
 
 @Component({
     selector: 'app-root',
+    standalone: true,
     imports: [RouterModule, CommonModule, BigLogoComponent],
     templateUrl: './app.component.html',
     encapsulation: ViewEncapsulation.None
--- apps/frontend/src/app/components/template/big-logo/big-logo.component.ts
+++ apps/frontend/src/app/components/template/big-logo/big-logo.component.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-big-logo',
+    standalone: true,
     imports: [CommonModule, RouterModule],
     templateUrl: './big-logo.component.html'
 })
