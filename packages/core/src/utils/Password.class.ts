3a40d7b3f12e2d77ac080737db4fc94eae4cb85d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-10 16:16:50 -0300

    feat: add condition for  password size

--- packages/core/src/utils/Password.class.ts
+++ packages/core/src/utils/Password.class.ts
@@ -1,8 +1,11 @@
 export default class Password {
   static new(size: number = 15): string {
+    if (size < 0) {
+      throw new Error("Tamanho da senha não pode ser negativo.");
+    }
     const characters =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-{}[]|\\:;"\'<>,.?/~`';
-    let password = '';
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-{}[]|\\:;\"'<>,.?/~`";
+    let password = "";
 
     for (let i = 0; i < size; i++) {
       password += characters.charAt(
