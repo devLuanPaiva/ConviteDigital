e35b84cad5f62ad3e1462b030161e870345e693a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-10 15:02:39 -0300

    feat: create message service

---
+++ apps/frontend/src/app/services/message.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  constructor(private readonly snack: MatSnackBar) {}
+
+  private showMessage(type: 'success' | 'error', text: string): void {
+    this.snack.open(
+      type === 'success' ? 'Tudo certo por aqui!' : 'Oops, algo deu errado!',
+      'Close',
+      {
+        duration: 5000,
+        panelClass: type === 'success' ? 'toast-success' : 'toast-error',
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      },
+    );
+  }
+
+  success(text: string): void {
+    this.showMessage('success', text);
+  }
+
+  error(text: string): void {
+    this.showMessage('error', text);
+  }
+}
