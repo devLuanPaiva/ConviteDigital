f077aefac2581d13f0653bd83f0d171207cc1d1e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-11 15:00:20 -0300

    feat: testing the return an empty string if input is empty

--- packages/core/src/__tests__/Alias.test.ts
+++ packages/core/src/__tests__/Alias.test.ts
@@ -1,13 +1,18 @@
 import { Alias } from "../utils";
 describe("Alias", () => {
   it("should replace spaces with hyphens and convert to lowercase", () => {
-    const input = 'Hello world!';
+    const input = "Hello world!";
     const output = Alias.format(input);
-    expect(output).toBe('hello-world!');
+    expect(output).toBe("hello-world!");
   });
-  it('should handle strings with multiple spaces correctly', () => {
-    const input = '  Multiple   Spaces   ';
+  it("should handle strings with multiple spaces correctly", () => {
+    const input = "  Multiple   Spaces   ";
     const output = Alias.format(input);
-    expect(output).toBe('--multiple---spaces---');
+    expect(output).toBe("--multiple---spaces---");
+  });
+  it("should return an empty string if input is empty", () => {
+    const input = "";
+    const output = Alias.format(input);
+    expect(output).toBe("");
   });
 });
