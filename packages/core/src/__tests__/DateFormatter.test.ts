24bc427590977670b6851372e7866a7a1e63999c
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-11 14:50:52 -0300

    feat: testing unformatting a valid string into a Date object

--- packages/core/src/__tests__/DateFormatter.test.ts
+++ packages/core/src/__tests__/DateFormatter.test.ts
@@ -7,14 +7,22 @@ describe("DateFormatter", () => {
     expect(formattedDate).toBe("2024-12-11T14:30");
   });
   it("should format the current date if no date is provided", () => {
-    const mockDate = new Date(2024, 11, 11, 14, 30); 
-    jest
-      .spyOn(global, "Date")
-      .mockImplementation(() => mockDate);
+    const mockDate = new Date(2024, 11, 11, 14, 30);
+    jest.spyOn(global, "Date").mockImplementation(() => mockDate);
 
     const formattedDate = DateFormatter.format(undefined as unknown as Date);
     expect(formattedDate).toBe("2024-12-11T14:30");
 
-    jest.restoreAllMocks(); 
+    jest.restoreAllMocks();
+  });
+  it("should unformat a valid string into a Date object", () => {
+    const formattedDate = "2024-12-11T14:30";
+    const date = DateFormatter.unformat(formattedDate);
+
+    expect(date.getFullYear()).toBe(2024);
+    expect(date.getMonth()).toBe(11);
+    expect(date.getDate()).toBe(11);
+    expect(date.getHours()).toBe(14);
+    expect(date.getMinutes()).toBe(30);
   });
 });
