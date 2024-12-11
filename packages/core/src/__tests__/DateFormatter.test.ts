7b854fee461feb7e9ae19c6cb7e657d73f97f139
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-11 14:49:11 -0300

    feat: testing formatting the current date if no date is provided

--- packages/core/src/__tests__/DateFormatter.test.ts
+++ packages/core/src/__tests__/DateFormatter.test.ts
@@ -4,6 +4,17 @@ describe("DateFormatter", () => {
   it("should format a given Date object correctly", () => {
     const date = new Date(2024, 11, 11, 14, 30);
     const formattedDate = DateFormatter.format(date);
-    expect(formattedDate).toBe('2024-12-11T14:30');
+    expect(formattedDate).toBe("2024-12-11T14:30");
+  });
+  it("should format the current date if no date is provided", () => {
+    const mockDate = new Date(2024, 11, 11, 14, 30); 
+    jest
+      .spyOn(global, "Date")
+      .mockImplementation(() => mockDate);
+
+    const formattedDate = DateFormatter.format(undefined as unknown as Date);
+    expect(formattedDate).toBe("2024-12-11T14:30");
+
+    jest.restoreAllMocks(); 
   });
 });
