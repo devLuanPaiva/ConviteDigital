19b4dee84176b637e337caa359f7b2d76ccec7c1
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-11 14:58:47 -0300

    feat: testing handling strings with multiple spaces correctly

--- packages/core/src/__tests__/Alias.test.ts
+++ packages/core/src/__tests__/Alias.test.ts
@@ -5,4 +5,9 @@ describe("Alias", () => {
     const output = Alias.format(input);
     expect(output).toBe('hello-world!');
   });
+  it('should handle strings with multiple spaces correctly', () => {
+    const input = '  Multiple   Spaces   ';
+    const output = Alias.format(input);
+    expect(output).toBe('--multiple---spaces---');
+  });
 });
