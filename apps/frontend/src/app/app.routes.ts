cc08f861ba3530a0dd905cf49473e9c26db4a5c0
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-14 14:27:22 -0300

    refactor: remove path to landing page

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,24 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { LandingComponent } from './pages/landing/landing.component';
-import { AppComponent } from './app.component';
 export const routes: Routes = [
-    { 
-        path: '', 
-        component: LandingComponent, 
-        pathMatch: 'full' 
-      },
-      { 
-        path: '', 
-        component: AppComponent,
-        children: [
-          {
-            path: 'event',
-            loadChildren: () => 
-              import('./pages/event/event.module').then(m => m.EventModule),
-          },
-        ],
-      },
+  {
+    path: 'event',
+    loadChildren: () => 
+      import('./pages/event/event.module').then(m => m.EventModule),
+  },
+  
+      
 ];
 
 @NgModule({
