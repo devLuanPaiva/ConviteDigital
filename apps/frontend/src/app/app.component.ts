d27b78459f554382eb590514095258b0e17a12c6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-14 14:29:46 -0300

    refactor: Condition the display in the HTML template

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { PageComponent } from './components/template/page/page.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [ PageComponent],
-    templateUrl: './app.component.html',
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-root',
+  standalone: true,
+  imports: [PageComponent, LandingComponent, CommonModule],
+  templateUrl: './app.component.html',
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public router: Router) {}
+}
