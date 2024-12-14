175d3629ef2b07d7d79767bfd6be63feb7d7d6e5
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-14 09:38:43 -0300

    refactor: import page template in the app component

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component, ViewEncapsulation } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { BigLogoComponent } from './components/template/big-logo/big-logo.component';
+import { PageComponent } from './components/template/page/page.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule, CommonModule, BigLogoComponent],
+    imports: [ PageComponent],
     templateUrl: './app.component.html',
     encapsulation: ViewEncapsulation.None
 })
