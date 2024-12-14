ce7564e17a825f2d5db14859c7f83059b57654ec
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-14 09:39:14 -0300

    refactor: implement lazy loading in app routes

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,8 +1,28 @@
-import { Routes } from '@angular/router';
-import {EventComponent} from './pages/event/event.component'
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LandingComponent } from './pages/landing/landing.component';
+import { AppComponent } from './app.component';
 export const routes: Routes = [
-    {
-        path: 'event',
-        component: EventComponent
-    }
+    { 
+        path: '', 
+        component: LandingComponent, 
+        pathMatch: 'full' 
+      },
+      { 
+        path: '', 
+        component: AppComponent,
+        children: [
+          {
+            path: 'event',
+            loadChildren: () => 
+              import('./pages/event/event.module').then(m => m.EventModule),
+          },
+        ],
+      },
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
