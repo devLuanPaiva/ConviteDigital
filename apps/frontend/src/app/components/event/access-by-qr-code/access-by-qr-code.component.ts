c1d6cc5a873498a1177ba4e62d80cfd480fe827b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-15 15:53:31 -0300

    refactor: update component

--- apps/frontend/src/app/components/event/access-by-qr-code/access-by-qr-code.component.ts
+++ apps/frontend/src/app/components/event/access-by-qr-code/access-by-qr-code.component.ts
@@ -1,20 +1,23 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { QRCodeComponent } from 'angularx-qrcode';
+import QRCode from 'qrcode';
 import { Event } from 'core';
 
 @Component({
   selector: 'app-access-by-qr-code',
-  imports: [QRCodeComponent, CommonModule],
   templateUrl: './access-by-qr-code.component.html',
 })
 export class AccessByQrCodeComponent {
   @Input() event: Partial<Event> = {};
+  qrCodeUrl: string = '';
 
-  get qrCodeValue(): string {
-    return JSON.stringify({
+  ngOnChanges(): void {
+    const qrData = JSON.stringify({
       id: this.event.id,
       password: this.event.password,
+    });
+
+    QRCode.toDataURL(qrData).then((url) => {
+      this.qrCodeUrl = url;
     });
   }
 }
