bcdd6a42aac384f32cb654ab289e3a6202d42c58
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-15 14:47:01 -0300

    refactor: import desired icons from Font Awesome

--- apps/frontend/src/app/pages/success/success.component.ts
+++ apps/frontend/src/app/pages/success/success.component.ts
@@ -1,11 +1,13 @@
+import { Event } from 'core';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../services/event.service';
+import { faLink, faKey } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { WindowComponent } from '../../components/shared/window/window.component';
 import { ClipboardComponent } from '../../components/shared/clipboard/clipboard.component';
 import { EventInformationComponent } from '../../components/event/event-information/event-information.component';
 import { AccessByQrCodeComponent } from '../../components/event/access-by-qr-code/access-by-qr-code.component';
-import { EventService } from '../../services/event.service';
-import { Event } from 'core';
 
 @Component({
   selector: 'app-success',
@@ -15,12 +17,15 @@ import { Event } from 'core';
     ClipboardComponent,
     EventInformationComponent,
     AccessByQrCodeComponent,
+    FontAwesomeModule,
   ],
   templateUrl: './success.component.html',
 })
 export class SuccessComponent implements OnInit {
   event: Partial<Event> = {};
-  currentURL: string = ''
+  currentURL: string = '';
+  faLink = faLink;
+  faKey = faKey;
   constructor(private readonly eventService: EventService) {}
 
   ngOnInit(): void {
