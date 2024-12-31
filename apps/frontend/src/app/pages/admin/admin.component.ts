bd2334de54abc8aa1eefef3ad5b491aaf8a0fa7d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-31 16:32:10 -0300

    refactor: implement admin page functions in another way

--- apps/frontend/src/app/pages/admin/admin.component.ts
+++ apps/frontend/src/app/pages/admin/admin.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Event, Guest } from 'core';
-import { EventService } from '../../services/event.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { DashboardEventComponent } from '../../components/event/dashboard-event/dashboard-event.component';
 import { EventPasswordFormComponent } from '../../components/event/event-password-form/event-password-form.component';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-admin',
@@ -18,44 +18,42 @@ export class AdminComponent implements OnInit {
   presents: Guest[] = [];
   absents: Guest[] = [];
   totalGuests: number = 0;
-  private subscription: Subscription = new Subscription();
+  subscriptions: Subscription = new Subscription();
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly eventService: EventService
+    private readonly apiService: ApiService,
   ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.password = this.route.snapshot.paramMap.get('password') || '';
-
-    this.loadEvent(id, this.password);
-
-    this.subscription = this.eventService.event$.subscribe((event) => {
-      if (event) {
-        this.event = event as Event; 
-        this.presents = this.event.guests.filter((g) => g.confirmed) || [];
-        this.absents = this.event.guests.filter((g) => !g.confirmed) || [];
-        this.totalGuests = this.presents.reduce(
-          (total, guest) => total + guest.numberOfCompanions + 1,
-          0
-        );
-      }
-    });
-  }
-
-
-  async loadEvent(id: string, password: string): Promise<void> {
-    await this.eventService.loadEvent(id);
-  }
-
-  async updateGuestList(): Promise<void> {
-    if (this.event) {
-      await this.eventService.loadEvent(this.event.id);
+    const password = this.route.snapshot.paramMap.get('password');
+    if (id && password) {
+      const decodedPassword = decodeURIComponent(password);
+      this.password = decodedPassword;
+      this.loadEvent(id, decodedPassword);
     }
   }
 
+  loadEvent(id: string, password: string): void {
+    const subscription = this.apiService
+      .httpPost('events/access', { id, password })
+      .subscribe((eventData: Event | null) => {
+        if (eventData) {
+          this.event = eventData;
+          this.presents = this.event.guests?.filter((g) => g.confirmed) || [];
+          this.absents = this.event.guests?.filter((g) => !g.confirmed) || [];
+          this.totalGuests = this.presents.reduce(
+            (total, guest) => total + (guest.numberOfCompanions || 0) + 1,
+            0,
+          );
+        }
+      });
+
+    this.subscriptions.add(subscription);
+  }
+
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.subscriptions.unsubscribe();
   }
 }
