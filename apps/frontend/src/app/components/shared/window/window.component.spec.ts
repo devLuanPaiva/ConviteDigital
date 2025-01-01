9153e9fcb314d01058354c61bff125b199719641
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-01-01 13:55:23 -0300

    feat: test to display default background image

---
+++ apps/frontend/src/app/components/shared/window/window.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WindowComponent } from './window.component';
+import { CommonModule } from '@angular/common';
+import { By } from '@angular/platform-browser';
+
+describe('WindowComponent', () => {
+  let component: WindowComponent;
+  let fixture: ComponentFixture<WindowComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CommonModule, WindowComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(WindowComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should display default background image if no input is provided', () => {
+    fixture.detectChanges();
+    const imgElement = fixture.debugElement.query(
+      By.css('img[alt="background"]'),
+    ).nativeElement;
+    expect(imgElement.src).toContain('EVP_0141-scaled.jpg');
+  });
+});
