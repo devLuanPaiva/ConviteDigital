e4c6cb7b719db0a55761a737c7aadf0c90477b5d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-01-01 10:08:01 -0300

    refactor: remove OnDestroy import

--- apps/frontend/src/app/pages/admin/admin.component.ts
+++ apps/frontend/src/app/pages/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Event, Guest } from 'core';
 import { Subscription } from 'rxjs';
