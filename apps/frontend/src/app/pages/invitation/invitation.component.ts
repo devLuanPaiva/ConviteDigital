109a182b5bc944bead2dc80c62d43e928f2a666e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-01-04 11:01:48 -0300

    feat: create invitation page

---
+++ apps/frontend/src/app/pages/invitation/invitation.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Event, Guest } from 'core';
+import { Subscription } from 'rxjs';
+import { EventService } from '../../services/event.service';
+import { ActivatedRoute } from '@angular/router';
+import { WindowComponent } from '../../components/shared/window/window.component';
+import { EventInformationComponent } from '../../components/event/event-information/event-information.component';
+import { FormGuestComponent } from '../../components/event/form-guest/form-guest.component';
+import { LoadingComponent } from '../../components/shared/loading/loading.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-invitation',
+  imports: [
+    WindowComponent,
+    EventInformationComponent,
+    FormGuestComponent,
+    LoadingComponent,
+    CommonModule,
+],
+  templateUrl: './invitation.component.html',
+})
+export class InvitationComponent implements OnInit {
+  event: Event | null = null;
+  guest: Guest | null = null;
+  subscriptions: Subscription = new Subscription();
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly eventService: EventService,
+  ) {}
+  async ngOnInit(): Promise<void> {
+    const alias = this.route.snapshot.paramMap.get('alias');
+    if (alias) {
+      try {
+        this.eventService.loadEvent(alias);
+        this.eventService.event$.subscribe((eventData) => {
+          if (eventData) {
+            this.event = eventData as Event;
+            this.guest = eventData.guests ? eventData.guests[0] : null;
+          }
+        });
+      } catch (error) {
+        console.error('Erro ao carregar o evento:', error);
+      }
+    }
+  }
+  changeGuest(guest: Partial<Guest>) {
+    this.eventService.toggleGuest(guest);
+  }
+  addGuest() {
+    this.eventService.addGuest();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+}
