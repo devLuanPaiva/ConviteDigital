039da8011b46a733f46811fc34049a3adc1e9419
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-01-05 16:01:14 -0300

    feat: test to display label when label input is provided

---
+++ apps/frontend/src/app/components/shared/input-boolean/input-boolean.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { InputBooleanComponent } from './input-boolean.component';
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
+describe('InputBooleanComponent', () => {
+  let component: InputBooleanComponent;
+  let fixture: ComponentFixture<InputBooleanComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InputBooleanComponent, CommonModule],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(InputBooleanComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should display label when label input is provided', () => {
+    component.label = 'Test Label';
+    fixture.detectChanges();
+
+    const labelElement = fixture.debugElement.query(By.css('label'));
+    expect(labelElement).toBeTruthy();
+    expect(labelElement.nativeElement.textContent).toBe('Test Label');
+  });
+});
