a65b347d6ab562192def6b087780143884ef345c
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-01-12 15:05:12 -0300

    feat: add openLink method

--- apps/frontend/src/app/components/shared/clipboard/clipboard.component.ts
+++ apps/frontend/src/app/components/shared/clipboard/clipboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MessageService } from '../../../services/message.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCopy, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faLink, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -15,10 +15,20 @@ export class ClipboardComponent {
   @Input() observation: string = '';
   @Input() icon?: IconDefinition;
   faCopy = faCopy;
+  faLink = faLink
   constructor(private readonly message: MessageService) {}
 
   textCopy() {
     navigator.clipboard.writeText(this.text);
     this.message.success('Texto copiado com sucesso!');
+  }
+  openLink(event: Event) {
+    event.preventDefault()
+    try {
+      const url = new URL(this.text); 
+      window.open(url.toString(), '_blank');
+    } catch {
+      this.message.error('Texto não é um link válido!');
+    }
   }
 }
